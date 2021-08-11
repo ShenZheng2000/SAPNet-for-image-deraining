dab897c0a4ebbcb58032aca680cccfc95b4468c3
ShenZheng2000 <69662345+ShenZheng2000@users.noreply.github.com>
2021-08-11 15:31:21 +0800

    Update train.py

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ from torch.utils.data import DataLoader
 from tensorboardX import SummaryWriter
 from Modeling.DerainDataset import *
 from Modeling.utils import *
+from Modeling.Contrastive.Mix import *
 from torch.optim.lr_scheduler import MultiStepLR
 from Modeling.SSIM import SSIM
 from Modeling.network import *
@@ -17,22 +18,61 @@ from Modeling.fpn import *
 
 parser = argparse.ArgumentParser(description="SAPNet_train")
 parser.add_argument("--preprocess", type=bool, default=True, help='run prepare_data or not')
-parser.add_argument("--batch_size", type=int, default=16, help="Training batch size")
+parser.add_argument("--batch_size", type=int, default=12, help="Training batch size") # default is 16
 parser.add_argument("--epochs", type=int, default=100, help="Number of training epochs")
 parser.add_argument("--milestone", type=int, default=[30, 50, 80], help="When to decay learning rate")
 parser.add_argument("--lr", type=float, default=1e-3, help="initial learning rate")
 parser.add_argument("--save_path", type=str, default="logs/SAPNet/Model1", help='path to save models and log files')
 parser.add_argument("--save_freq", type=int, default=1, help='save intermediate model')
 parser.add_argument("--data_path", type=str, default="datasets/train/RainTrainH", help='path to training data')
-parser.add_argument("--use_gpu", type=bool, default=False, help='use GPU or not')  # Set True for GPU training
+parser.add_argument("--use_contrast", type=bool, default=True, help='use contrasive regularization or not')
 parser.add_argument("--gpu_id", type=str, default="0", help='GPU id')
 parser.add_argument("--recurrent_iter", type=int, default=6, help='number of recursive stages')
 # add for segmentation model
 parser.add_argument("--num_of_SegClass", type=int, default=21, help='Number of Segmentation Classes, default VOC = 21')
+
 opt = parser.parse_args()
 
-if opt.use_gpu:
+if torch.cuda.is_available():
     os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpu_id
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+################## USRA block starts here ################
+def SegLoss(input_train, out_train):
+    # TODO: (1) Seg is too large, consider scale with 0.1  (2) Try Segment the Image, not the rain residual
+    # Build Segmentation model
+    seg = fpn(opt.num_of_SegClass).to(device)
+    seg_criterion = FocalLoss(gamma=2).to(device)
+
+    # Build and clip residual image
+    out_train = torch.clamp(input_train - out_train, 0., 1.)
+
+    # Build and dmean seg. input (maybe clip image before)
+    seg_input = out_train.data.cpu().numpy()
+    for n in range(out_train.size()[0]):
+        seg_input[n, :, :, :] = rgb_demean(seg_input[n, :, :, :])
+
+    # send seg. input to cuda
+    seg_input = Variable(torch.from_numpy(seg_input).to(device))
+
+    # build seg. output
+    seg_output = seg(seg_input)
+
+    # build seg. target
+    target = (get_NoGT_target(seg_output)).data.cpu()
+    target_ = resize_target(target, seg_output.size(2))
+    target_ = torch.from_numpy(target_).long().to(device)
+
+    # calculate seg. loss
+    seg_loss = seg_criterion(seg_output, target_).to(device)
+
+    # freeze seg. backpropagation
+    for param in seg.parameters():
+        param.requires_grad = False
+
+    return seg_loss
+
 
 
 def main():
@@ -44,24 +84,12 @@ def main():
     print("# of training samples: %d\n" % int(len(dataset_train)))
 
     # Build deraining model
-    model = SAPNet(recurrent_iter=opt.recurrent_iter, use_GPU=opt.use_gpu)
+    model = SAPNet(recurrent_iter=opt.recurrent_iter,
+                   use_Contrast=opt.use_contrast).to(device)
     print_network(model)
 
-    # Build Segmentation model
-    seg = fpn(opt.num_of_SegClass)
-    seg_criterion = FocalLoss(gamma=2)
-    # seg = nn.DataParallel(seg)
-
-    # loss function
-    # criterion = nn.MSELoss(size_average=False)
-    criterion = SSIM()
-
-    # Move models and criterions to GPU
-    if opt.use_gpu:
-        model = model.cuda()
-        criterion.cuda()
-        seg_criterion = seg_criterion.cuda()
-        seg = seg.cuda()
+    # Define SSIM
+    criterion = SSIM().to(device)
 
     # Optimizer
     optimizer = optim.Adam(model.parameters(), lr=opt.lr)
@@ -89,58 +117,21 @@ def main():
             model.zero_grad()
             optimizer.zero_grad()
 
-            input_train, target_train = Variable(input_train), Variable(target_train)
-
-            if opt.use_gpu:
-                input_train, target_train = input_train.cuda(), target_train.cuda()
+            input_train, target_train = Variable(input_train).to(device), Variable(target_train).to(device)
 
             # Obtain the derained image and calculate ssim loss
-            #out_train, _ = model(input_train)
             out_train, _ = model(input_train)
 
             pixel_metric = criterion(target_train, out_train)
 
-            ################### Calculating edges starts here ###################
-            #edge_loss = laplacian_loss(target_train, out_train)
-
-            # SSIM and edge loss
-            #loss = -pixel_metric + 0.05 * edge_loss
+            # Negative SSIM loss
             loss = -pixel_metric
+            print("ssim loss is", loss)
 
-            #loss = -pixel_metric
+            # Segmentation loss
+            seg_loss = SegLoss(input_train, out_train)
+            print("seg loss is", seg_loss)
 
-            ################## USRA block starts here ################
-            # Build and clip residual image
-            out_train = torch.clamp(input_train - out_train, 0., 1.)
-
-            # Build and dmean seg. input (maybe clip image before)
-            seg_input = out_train.data.cpu().numpy()
-            for n in range(out_train.size()[0]):
-                seg_input[n, :, :, :] = rgb_demean(seg_input[n, :, :, :])
-
-            # send seg. input to cuda
-            if opt.use_gpu:
-                seg_input = Variable(torch.from_numpy(seg_input).cuda())
-            else:
-                seg_input = Variable(torch.from_numpy(seg_input))
-
-            # build seg. output
-            seg_output = seg(seg_input)
-
-            # build seg. target
-            target = (get_NoGT_target(seg_output)).data.cpu()
-            target_ = resize_target(target, seg_output.size(2))
-            target_ = torch.from_numpy(target_).long()
-
-            if opt.use_gpu:
-                target_ = target_.cuda()
-
-            # calculate seg. loss
-            seg_loss = seg_criterion(seg_output, target_)
-
-            # freeze seg. backpropagation
-            for param in seg.parameters():
-                param.requires_grad = False
 
             # calculate total loss
             total_loss = loss + seg_loss
@@ -149,16 +140,13 @@ def main():
             total_loss.backward()
             optimizer.step()
 
-            ####### Right now Keep this part unmodified
 
-            # training curve
+
+            ####### Right now Keep this part unmodified
             model.eval()
-            #out_train, _ = model(input_train)
             out_train, _ = model(input_train)
             out_train = torch.clamp(out_train, 0., 1.)
             psnr_train = batch_PSNR(out_train, target_train, 1.)
-            #print("[epoch %d][%d/%d] loss: %.4f, pixel_metric: %.4f, PSNR: %.4f" %
-                  #(epoch+1, i+1, len(loader_train), loss.item(), pixel_metric.item(), psnr_train))
             print("[epoch %d][%d/%d] loss: %.4f, pixel_metric: %.4f, PSNR: %.4f" %
                   (epoch + 1, i + 1, len(loader_train), total_loss.item(), pixel_metric.item(), psnr_train))
 
@@ -167,11 +155,9 @@ def main():
                 writer.add_scalar('loss', total_loss.item(), step)
                 writer.add_scalar('PSNR on training data', psnr_train, step)
             step += 1
-        # epoch training end
 
         # log the images
         model.eval()
-        #out_train, _ = model(input_train)
         out_train, _ = model(input_train)
         out_train = torch.clamp(out_train, 0., 1.)
         im_target = utils.make_grid(target_train.data, nrow=8, normalize=True, scale_each=True)
