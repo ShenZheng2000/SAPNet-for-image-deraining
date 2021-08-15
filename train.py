2bef95a1b9a2802257795f61e6afb1549bc6a291
ShenZheng2000 <69662345+ShenZheng2000@users.noreply.github.com>
2021-08-15 23:04:52 +0800

    Update train.py

--- train.py
+++ train.py
@@ -20,14 +20,14 @@ from torchvision import datasets, transforms
 parser = argparse.ArgumentParser(description="SAPNet_train")
 parser.add_argument("--gpu_id", type=str, default="0", help='GPU id')
 parser.add_argument("--preprocess", type=bool, default=False, help='run prepare_data or not')
-parser.add_argument("--batch_size", type=int, default=8, help="Training batch size") # default is 16
+parser.add_argument("--batch_size", type=int, default=4, help="Training batch size") # default is 16
 parser.add_argument("--epochs", type=int, default=100, help="Number of training epochs")
 parser.add_argument("--milestone", type=int, default=[30, 50, 80], help="When to decay learning rate")
 parser.add_argument("--lr", type=float, default=1e-3, help="initial learning rate")
 parser.add_argument("--save_path", type=str, default="logs/SAPNet/Model1", help='path to save models and log files')
 parser.add_argument("--save_freq", type=int, default=1, help='save intermediate model')
 parser.add_argument("--data_path", type=str, default="datasets/train/RainTrainH", help='path to training data')
-parser.add_argument("--data_path_real", type=str, default="D:/Code/AAAI_2022/datasets/real_input", help='path to training data of real rain')
+parser.add_argument("--data_path_real", type=str, default="C:/Users/yons/Desktop/CODE/AAAI_2022/datasets/real_input", help='path to training data of real rain')
 
 parser.add_argument("--use_contrast", type=bool, default=True, help='use contrasive regularization or not')
 parser.add_argument("--use_stage1", type=bool, default=True, help='use stage1: train on synthesize image or not')
@@ -42,6 +42,7 @@ if torch.cuda.is_available():
     os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpu_id
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
 
 ################## USRA block starts here ################
 def SegLoss(input_train, out_train):
