81b8692e1300c15fe0cc8b0d72a5382effe5d4b5
ShenZheng2000 <69662345+ShenZheng2000@users.noreply.github.com>
2021-08-25 09:36:55 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,53 +4,47 @@ from Modeling.network import *
 import time
 from option import *
 
-# TODO: change some parameters (be cons. with train.py)
 def test():
-    if torch.cuda.is_available():
-        os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpu_id
+    '''if torch.cuda.is_available():
+        os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpu_id'''
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    os.makedirs(opt.save_path, exist_ok=True)
+    os.makedirs(opt.output_path, exist_ok=True)
 
     # Build model
     print('Loading model ...\n')
-    model = SAPNet(recurrent_iter=opt.recurrent_iter,
-                   use_Contrast=opt.use_contrast).to(device)
+    model = SAPNet(recurrent_iter=opt.recurrent_iter, use_ghost=opt.use_ghost).to(device)
     print_network(model)
-    if opt.use_GPU:
-        model = model.cuda()
-    model.load_state_dict(torch.load(os.path.join(opt.logdir, 'net_latest.pth'), map_location=device))
+    model = model.to(device)
+    model = nn.DataParallel(model, device_ids=device_ids)
+    model.load_state_dict(torch.load(os.path.join(opt.save_path, 'net_latest.pth'), map_location=device))
     model.eval()
 
     time_test = 0
     count = 0
-    for img_name in os.listdir(opt.data_path):
+    for img_name in os.listdir(opt.test_data_path):
         if is_image(img_name):
-            img_path = os.path.join(opt.data_path, img_name)
+            img_path = os.path.join(opt.test_data_path, img_name)
 
             # input image
             y = cv2.imread(img_path)
             b, g, r = cv2.split(y)
             y = cv2.merge([r, g, b])
-            #y = cv2.resize(y, (int(500), int(500)), interpolation=cv2.INTER_CUBIC)
 
             y = normalize(np.float32(y))
             y = np.expand_dims(y.transpose(2, 0, 1), 0)
-            y = Variable(torch.Tensor(y))
-
-            if opt.use_GPU:
-                y = y.cuda()
+            y = Variable(torch.Tensor(y)).to(device)
 
             with torch.no_grad(): #
-                if opt.use_GPU:
+                if torch.cuda.is_available():
                     torch.cuda.synchronize()
                 start_time = time.time()
 
                 out, _ = model(y)
                 out = torch.clamp(out, 0., 1.)
 
-                if opt.use_GPU:
+                if torch.cuda.is_available():
                     torch.cuda.synchronize()
                 end_time = time.time()
                 dur_time = end_time - start_time
@@ -58,7 +52,7 @@ def test():
 
                 print(img_name, ': ', dur_time)
 
-            if opt.use_GPU:
+            if torch.cuda.is_available():
                 save_out = np.uint8(255 * out.data.cpu().numpy().squeeze())   #back to cpu
             else:
                 save_out = np.uint8(255 * out.data.numpy().squeeze())
@@ -67,7 +61,7 @@ def test():
             b, g, r = cv2.split(save_out)
             save_out = cv2.merge([r, g, b])
 
-            cv2.imwrite(os.path.join(opt.save_path, img_name), save_out)
+            cv2.imwrite(os.path.join(opt.output_path, img_name), save_out)
 
             count += 1
 
