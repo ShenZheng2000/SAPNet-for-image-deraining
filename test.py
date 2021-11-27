5cf020b9f3104d73fac841d8f39eefa299086c9c
ShenZheng2000 <69662345+ShenZheng2000@users.noreply.github.com>
2021-11-27 12:55:36 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,8 +5,6 @@ import time
 from option import *
 
 def test():
-    '''if torch.cuda.is_available():
-        os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpu_id'''
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     device_ids = [Id for Id in range(torch.cuda.device_count())]
@@ -16,8 +14,7 @@ def test():
     # Build model
     print('Loading model ...\n')
     model = SAPNet(recurrent_iter=opt.recurrent_iter,
-                   use_dilation=opt.use_dilation,
-                   use_split=opt.use_split).to(device)
+                   use_dilation=opt.use_dilation).to(device)
     print_network(model)
     model = model.to(device)
     model = nn.DataParallel(model, device_ids=device_ids)
